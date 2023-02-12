a25074bee4243b68a9c42a3dd1d0d3b27881f7c4
honeydot <108961980+honeydot@users.noreply.github.com>
2023-02-12 14:17:38 +0800

    Add files via upload

---
+++ 88/88.cpp
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#define Max(x,y) (x>y?x:y)
+int main()
+{
+	int a = 3121;
+	int b = 3213;
+	int max = Max(a, b);
+	printf("max=%d", max);
+	return 0;
+}
